5b5f69dc0c52c6b15d7b95b8a9e5c35c091853e2
Antony Guinard <antony.guinard@gmail.com>
2017-11-11 02:05:29 +0000

    Generated output from swagger-codegen

---
+++ api/public_api.rb
@@ -0,0 +1,62 @@
+require 'json'
+
+
+MyApp.add_route('POST', '/gantony/CorkDevQuestions/1.0.0/questions', {
+  "resourcePath" => "/Public",
+  "summary" => "new question",
+  "nickname" => "add_question", 
+  "responseClass" => "Question", 
+  "endpoint" => "/questions", 
+  "notes" => "Adds a question to the talk",
+  "parameters" => [
+    {
+      "name" => "body",
+      "description" => "Question to add",
+      "dataType" => "Question",
+      "paramType" => "body",
+    }
+    ]}) do
+  cross_origin
+  # the guts live here
+
+  {"message" => "yes, it worked"}.to_json
+end
+
+
+MyApp.add_route('GET', '/gantony/CorkDevQuestions/1.0.0/questions', {
+  "resourcePath" => "/Public",
+  "summary" => "questions",
+  "nickname" => "list_questions", 
+  "responseClass" => "array[Question]", 
+  "endpoint" => "/questions", 
+  "notes" => "Get a list of all the available questions for this talk ",
+  "parameters" => [
+    ]}) do
+  cross_origin
+  # the guts live here
+
+  {"message" => "yes, it worked"}.to_json
+end
+
+
+MyApp.add_route('POST', '/gantony/CorkDevQuestions/1.0.0/questions/{id}/votes', {
+  "resourcePath" => "/Public",
+  "summary" => "upvote",
+  "nickname" => "upvote", 
+  "responseClass" => "int", 
+  "endpoint" => "/questions/{id}/votes", 
+  "notes" => "Upvote a question for the talk so that it goes up the list (we may not have time to answer all questions)",
+  "parameters" => [
+    {
+      "name" => "id",
+      "description" => "ID of question to upvote",
+      "dataType" => "int",
+      "paramType" => "path",
+    },
+    ]}) do
+  cross_origin
+  # the guts live here
+
+  {"message" => "yes, it worked"}.to_json
+end
+
