27f99d107e75e91cdc55d2f7efcae905f2645675
Antony Guinard <antony.guinard@gmail.com>
2017-11-13 16:26:42 +0000

    Add CORS configuration for absolutely everything

--- my_app.rb
+++ my_app.rb
@@ -3,12 +3,31 @@ require './my_app_logic'
 
 # only need to extend if you want special configuration!
 class MyApp < Swaggering
+  
+  set :bind, '0.0.0.0'
+
   self.configure do |config|
+    enable :cross_origin
     config.api_version = '1.0.0' 
+  end
+
+  before do
+    response.headers['Access-control-Allow-Origin'] = '*'
+  end
+
+  options "*" do
+    response.headers["Allow"] = "GET, POST, OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type, Accept, X-User-Email, X-Auth-Token"
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    200
   end
 
   set :public_folder, '../corkdev-questions-swagger-client/public'
   set :bind, '0.0.0.0'
+
+  get '/' do
+    redirect '/index.html'
+  end
 end
 
 # include the api files
