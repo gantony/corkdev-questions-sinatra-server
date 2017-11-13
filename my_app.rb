831489df961b5f7878d85cfdc6f66ad44d819721
Antony Guinard <antony.guinard@gmail.com>
2017-11-13 00:21:39 +0000

    Server static files from corkdev-questions-swagger-client repository

--- my_app.rb
+++ my_app.rb
@@ -6,6 +6,9 @@ class MyApp < Swaggering
   self.configure do |config|
     config.api_version = '1.0.0' 
   end
+
+  set :public_folder, '../corkdev-questions-swagger-client/public'
+  set :bind, '0.0.0.0'
 end
 
 # include the api files
